a67398bbd94fbfb308bf25c494f6a44a356fb431
cocosongying <cocosongying@gmail.com>
2020-04-29 18:09:28 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//import css
+import 'vue-beauty/package/style/vue-beauty.min.css'
+
+//import components
+import vueBeauty from 'vue-beauty'
+Vue.use(vueBeauty)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import CalRelations from '@/components/other/CalRelations'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/tools',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/calrelations',
+      component: CalRelations
     }
   ]
 })
